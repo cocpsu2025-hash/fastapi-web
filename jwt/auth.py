d0a46dbc24cf9fccd179aa8009e4df3e5bc17d3f
CoC PSU 2025 <cocpsu2025@gmail.com>
2026-09-11 14:01:25 +0700

    add hashpassword

--- jwt/auth.py
+++ jwt/auth.py
@@ -1,4 +1,6 @@
 
+import datetime
+from pwdlib import PasswordHash
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 import jwt
@@ -9,11 +11,14 @@ SECRET_KEY = "my-secret-key"
 ALGORITHM = "HS256"
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
+password_hash = PasswordHash.recommended()
+hashed = password_hash.hash("1234")
+
 users = {
     "admin":
     { 
         "username": "admin",
-        "password": "1234"
+        "password": hashed
     }
 }
 
@@ -25,13 +30,17 @@ async def login( form_data: OAuth2PasswordRequestForm = Depends()):
 
     print(user["password"])
 
-    if not user or not user["password"] == form_data.password:
+    # if not user or not user["password"] == form_data.password:
+    if not user or not password_hash.verify(form_data.password, user["password"]):
         raise HTTPException(status_code=401, detail="Incorrect username or password")
    
     print(user)
 
+    expire = datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=1)
+
     token = jwt.encode( {
-        "sub": user["username"]
+        "sub": user["username"],
+        "exp": expire # Optional
     }, 
     SECRET_KEY, algorithm=ALGORITHM
     )
